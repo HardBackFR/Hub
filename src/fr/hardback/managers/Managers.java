4b337dd6b32e22c7c833546874ae0848c403705e
HardBackMC <98814719+HardBackMC@users.noreply.github.com>
2022-02-15 21:20:47 +0100

    :cyclone: udpate

--- src/fr/hardback/managers/Managers.java
+++ src/fr/hardback/managers/Managers.java
@@ -1,7 +1,6 @@
 package fr.hardback.managers;
 
 import fr.hardback.Hub;
-import fr.hardback.managers.commands.CommandChest;
 import fr.hardback.managers.listeners.CancelListener;
 import fr.hardback.managers.listeners.PlayerListener;
 import org.bukkit.plugin.PluginManager;
@@ -17,7 +16,5 @@ public class Managers {
 
         pluginManager.registerEvents(new PlayerListener(this.instance), this.instance);
         pluginManager.registerEvents(new CancelListener(this.instance), this.instance);
-
-        this.instance.getCommand("test").setExecutor(new CommandChest());
     }
 }
--- src/fr/hardback/utils/inventory/gui/main/GuiMain.java
+++ src/fr/hardback/utils/inventory/gui/main/GuiMain.java
@@ -1,6 +1,7 @@
 package fr.hardback.utils.inventory.gui.main;
 
 import fr.hardback.Hub;
+import fr.hardback.commons.DatabaseManager;
 import fr.hardback.spigot.tools.gui.AbstractGui;
 import fr.hardback.spigot.tools.item.ItemBuilder;
 import fr.hardback.utils.inventory.gui.cosmetics.GuiCosmetics;
@@ -29,9 +30,10 @@ public class GuiMain extends AbstractGui {
 
     @Override
     public void display(){
-        //PluginMessaging.requestCount("survie", getPlayer());
-        //PluginMessaging.requestCount("event-corp", getPlayer());
-        //PluginMessaging.requestCount("ffa", getPlayer());
+        /*
+        PluginMessaging.requestCount("evolution", getPlayer());
+        PluginMessaging.requestCount("killskill", getPlayer());
+         */
         final Inventory inventory = Bukkit.createInventory(null, 9 * 6, "Menu Principal");
 
         inventory.setItem(0, new ItemBuilder(Material.STAINED_GLASS_PANE, 1, (byte) 0).setName(" ").toItemStack());
@@ -42,11 +44,9 @@ public class GuiMain extends AbstractGui {
         inventory.setItem(8, new ItemBuilder(Material.STAINED_GLASS_PANE, 1, (byte) 0).setName(" ").toItemStack());
         inventory.setItem(17, new ItemBuilder(Material.STAINED_GLASS_PANE, 1, (byte) 0).setName(" ").toItemStack());
 
-        inventory.setItem(13, new ItemBuilder(Material.SIGN).setName(ChatColor.GREEN + "Evénement(s)").setLore(Arrays.asList(ChatColor.DARK_GRAY + "Event", " ", ChatColor.RED + "En maintenance...")).toItemStack());//inventory.setItem(13, new ItemBuilder(Material.SIGN).setName(ChatColor.GREEN + "Evénement(s)").setLore(Arrays.asList(ChatColor.DARK_GRAY + "Event", " ", ChatColor.WHITE + "" + ChatColor.BOLD + "Description", " ", ChatColor.GRAY + " Serveur pour les évents avec la corporation !", " ", ChatColor.YELLOW + "" + ChatColor.BOLD + "Informations", ChatColor.GRAY + "-> Joueurs connectés: " + ChatColor.GREEN + PluginMessaging.getPlayerCount("event-corp") + ChatColor.DARK_GRAY + "/375", ChatColor.GRAY + "-> Version du serveur: " + ChatColor.YELLOW + "1.8", " ", ChatColor.YELLOW + "» " + ChatColor.GREEN + "Cliquez " + ChatColor.GRAY + "pour rejoindre")).toItemStack());
-        inventory.setItem(20, new ItemBuilder(Material.NETHER_STAR).setName(ChatColor.GREEN + "Evolution").setLore(Arrays.asList(ChatColor.DARK_GRAY + "Equipes, PVP", " ", ChatColor.RED + "En maintenance...")).toItemStack());
-        inventory.setItem(22, new ItemBuilder(Material.DIAMOND_SWORD).setName(ChatColor.GREEN + "FFA").setLore(Arrays.asList(ChatColor.DARK_GRAY + "Solo, PVP", " ", ChatColor.RED + "En maintenance...")).toItemStack());//inventory.setItem(22, new ItemBuilder(Material.DIAMOND_SWORD).setName(ChatColor.GREEN + "FFA").setLore(Arrays.asList(ChatColor.DARK_GRAY + "Solo, PVP", " ", ChatColor.WHITE + "" + ChatColor.BOLD + "Description", " ", ChatColor.GRAY + " Un FFA basique, bagarre.. DU SANG", " ", ChatColor.YELLOW + "" + ChatColor.BOLD + "Informations", ChatColor.GRAY + "-> Joueurs connectés: " + ChatColor.GREEN + PluginMessaging.getPlayerCount("ffa") + ChatColor.DARK_GRAY + "/375", ChatColor.GRAY + "-> Version du serveur: " + ChatColor.YELLOW + "1.8", " ", ChatColor.YELLOW + "» " + ChatColor.GREEN + "Cliquez " + ChatColor.GRAY + "pour rejoindre")).toItemStack());
-        inventory.setItem(31, new ItemBuilder(Material.GRASS).setName(ChatColor.GREEN + "Survie").setLore(Arrays.asList(ChatColor.DARK_GRAY + "Survie, PVP", " ", ChatColor.RED + "En maintenance...")).toItemStack());//inventory.setItem(31, new ItemBuilder(Material.GRASS).setName(ChatColor.GREEN + "Survie").setLore(Arrays.asList(ChatColor.DARK_GRAY + "Survie, PVP", " ", ChatColor.WHITE + "" + ChatColor.BOLD + "Description", " ", ChatColor.GRAY + " Serveur survie, farm, construit, amuse-toi !", " ", ChatColor.YELLOW + "" + ChatColor.BOLD + "Informations", ChatColor.GRAY + "-> Joueurs connectés: " + ChatColor.GREEN + PluginMessaging.getPlayerCount("survie") + ChatColor.DARK_GRAY + "/375", ChatColor.GRAY + "-> Version du serveur: " + ChatColor.YELLOW + "1.8", " ", ChatColor.YELLOW + "» " + ChatColor.GREEN + "Cliquez " + ChatColor.GRAY + "pour rejoindre")).toItemStack());
-        inventory.setItem(24, new ItemBuilder(Material.ANVIL).setName(ChatColor.GREEN + "KillSkill").setLore(Arrays.asList(ChatColor.DARK_GRAY + "Solo, PVP", " ", ChatColor.RED + "En maintenance...")).toItemStack());
+        if(DatabaseManager.REDIS.getAccountData(getPlayer().getUniqueId()).getRank().isStaff()) inventory.setItem(13, new ItemBuilder(Material.BARRIER).setName(ChatColor.DARK_RED + "Serveur STAFF").setLore(Arrays.asList("", ChatColor.RED + "Seulement les membres du STAFF on le droit à l'accès à ce serveur !")).toItemStack());
+        inventory.setItem(20, new ItemBuilder(Material.NETHER_STAR).setName(ChatColor.GREEN + "Evolution").setLore(Arrays.asList(ChatColor.DARK_GRAY + "PVP, Equipe", " ", ChatColor.WHITE + "" + ChatColor.BOLD + "Description", " ", ChatColor.GRAY + " En développement !", " ", ChatColor.YELLOW + "" + ChatColor.BOLD + "Informations", ChatColor.GRAY + "-> Joueurs connectés: " + ChatColor.GREEN + PluginMessaging.getPlayerCount("evolution") + ChatColor.DARK_GRAY + "/375", ChatColor.GRAY + "-> Version du serveur: " + ChatColor.YELLOW + "1.8", " ", ChatColor.YELLOW + "» " + ChatColor.GREEN + "Cliquez " + ChatColor.GRAY + "pour rejoindre")).toItemStack());
+        inventory.setItem(24, new ItemBuilder(Material.ANVIL).setName(ChatColor.GREEN + "KillSkill").setLore(Arrays.asList(ChatColor.DARK_GRAY + "PVP, Solo", " ", ChatColor.WHITE + "" + ChatColor.BOLD + "Description", " ", ChatColor.GRAY + " En développement !", " ", ChatColor.YELLOW + "" + ChatColor.BOLD + "Informations", ChatColor.GRAY + "-> Joueurs connectés: " + ChatColor.GREEN + PluginMessaging.getPlayerCount("killskill") + ChatColor.DARK_GRAY + "/375", ChatColor.GRAY + "-> Version du serveur: " + ChatColor.YELLOW + "1.8", " ", ChatColor.YELLOW + "» " + ChatColor.GREEN + "Cliquez " + ChatColor.GRAY + "pour rejoindre")).toItemStack());
         inventory.setItem(48, new ItemBuilder(Material.SKULL_ITEM, 1, (byte) 3).setSkullOwner(this.getPlayer().getName()).setName(ChatColor.AQUA + "Profil").setLore(Collections.singletonList(ChatColor.GRAY + "Cliquer pour accéder à votre profil")).toItemStack());
         inventory.setItem(48, new ItemBuilder(Material.GOLD_INGOT).setName(ChatColor.AQUA + "Boutique").setLore(Collections.singletonList(ChatColor.GRAY + "Cliquer pour accéder à la boutique")).toItemStack());
         inventory.setItem(50, new ItemBuilder(Material.ENDER_CHEST).setName(ChatColor.AQUA + "Cosmétiques").setLore(Collections.singletonList(ChatColor.GRAY + "Cliquer pour accéder à vos cosmétiques")).toItemStack());
@@ -81,18 +81,12 @@ public class GuiMain extends AbstractGui {
             }else if(itemStack.getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GREEN + "KillSkill")) {
                 player.closeInventory();
                 PluginMessaging.connect("killskill", player);
-            }else if(itemStack.getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GREEN + "FFA")) {
+            }else if(itemStack.getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GREEN + "KillSkill")) {
                 player.closeInventory();
-                PluginMessaging.connect("ffa", player);
-            } else if(itemStack.getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GREEN + "Evénement(s)")) {
+                PluginMessaging.connect("killskill", player);
+            }else if(itemStack.getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.DARK_RED + "Serveur STAFF")) {
                 player.closeInventory();
-                PluginMessaging.connect("event-corp", player);
-            }else if(itemStack.getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GREEN + "Survie")) {
-                player.closeInventory();
-                PluginMessaging.connect("survie", player);
-            }else if(itemStack.getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.AQUA + "Boutique")) {
-                player.closeInventory();
-                new GuiShop(this.instance, player).display();
+                PluginMessaging.connect("staff-server", player);
             }else if(itemStack.getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.AQUA + "Cosmétiques")) {
                 player.closeInventory();
                 new GuiCosmetics(this.instance, player).display();
